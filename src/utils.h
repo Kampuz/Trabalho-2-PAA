08e96ff09a079273d64979e3fb587ca784911d43
Miguel de Campos Rodrigues Moret <miguel.moret@unesp.br>
2024-12-08 18:18:12 -0300

    feat: documented  the codes

--- src/utils.h
+++ src/utils.h
@@ -1,8 +1,17 @@
 #ifndef UTILS_
 #define UTILS_
 
+/**
+ *  @brief Pauses the program and waits for the user to press [ENTER].
+ */
 void proceed();
 
+
+/**
+ *  @brief Displays a menu for problem selection and captures the user's choice.
+ * 
+ *  @param option Pointer to an integer where the selected menu option will be stored.
+ */
 void menu(int *option);
 
 #endif
